6e984438f91d1bc87bc6a02f78d8f4fe5029d749
jair manosalva <yayomanosalva@gmail.com>
2016-05-28 12:01:54 -0500

    Update app.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,8 @@
 // Google Maps JavaScript API
+var map;
+
 function Mapa() {
+    'use strict'; // turn on Strict Mode
     /* ========= class for the Map function =========*/
     var myLatLng = {
         lat: 11.00414,
@@ -17,6 +20,8 @@ function Mapa() {
 
     var map = new google.maps.Map(document.getElementById('map'), myOptions);
 
+    var infowindow = new google.maps.InfoWindow({});
+
     /* ========= Marker function, so define the markers =========*/
     this.Marker = function(name, lat, long, category) {
         var self = this;
@@ -24,9 +29,10 @@ function Mapa() {
         self.lat = ko.observable(lat);
         self.long = ko.observable(long);
         self.category = ko.observable(category);
+        var contentString = '<div id="content" style="width:250px;height:280px;"></div>';
         /* ========= Images Icons  =========*/
         // Setup the different icons
-
+        //images restaurants
         var img = {
             url: 'images/restaurants.png',
             // This marker is 20 pixels wide by 32 pixels high.
@@ -36,16 +42,12 @@ function Mapa() {
             // The anchor for this image is the base of the flagpole at (0, 32).
             anchor: new google.maps.Point(0, 32)
         };
+        //images store
         var img2 = {
             url: 'images/store.png'
-        }
+        };
 
-        var contentString = '<div id="content" style="width:250px;height:300px;"></div>';
-
-        var infowindow = new google.maps.InfoWindow({
-            content: contentString
-        });
-
+        //marker
         var marker = new google.maps.Marker({
             position: new google.maps.LatLng(lat, long),
             title: name,
@@ -55,48 +57,34 @@ function Mapa() {
             animation: google.maps.Animation.DROP
         });
 
+        //click event close and open streetview
         marker.addListener('click', function() {
             if (infowindow) {
                 infowindow.close();
             }
-            infowindow = true;
-            infowindow.open(map, marker, toggleBounce);
-            map.setZoom(19);
+            infowindow.setContent(contentString);
+            infowindow.open(map, this);
+            map.setZoom(18);
             map.setCenter(marker.getPosition());
         });
 
+        //function click knockout list view name
+        //Click on item in list view
+        this.listViewClick = function() {
+            map.setZoom(18);
+            infowindow.setContent(contentString);
+            map.setCenter(marker.getPosition());
+            infowindow.open(map, marker);
+        };
+
+        //Animation marker
         function toggleBounce() {
             if (marker.getAnimation() !== null) {
                 marker.setAnimation(null);
             } else {
                 marker.setAnimation(google.maps.Animation.BOUNCE);
             }
-        }
-
-        var pano = null;
-        google.maps.event.addListener(infowindow, 'domready', function() {
-            if (pano != null) {
-                pano.unbind("position");
-                pano.setVisible(false);
-            }
-            pano = new google.maps.StreetViewPanorama(document.getElementById("content"), {
-                navigationControl: true,
-                navigationControlOptions: {
-                    style: google.maps.NavigationControlStyle.ANDROID
-                },
-                enableCloseButton: false,
-                addressControl: false,
-                linksControl: false
-            });
-            pano.bindTo("position", marker);
-            pano.setVisible(true);
-        });
-
-        google.maps.event.addListener(infowindow, 'closeclick', function() {
-            pano.unbind("position");
-            pano.setVisible(false);
-            pano = null;
-        });
+        };
     }
 
     /* ========= Array knockout js =========*/
@@ -113,23 +101,40 @@ function Mapa() {
     this.query = ko.observable('');
     /* object to hold our map instance  */
     this.search = ko.computed(function() {
-        return ko.utils.arrayFilter(this.locations(), function(i) {
-            return i.name().toLowerCase().indexOf(this.query().toLowerCase()) >= 0;
+        return ko.utils.arrayFilter(locations(), function(i) {
+            return i.name().toLowerCase().indexOf(query().toLowerCase()) >= 0;
         });
     });
+    //show streetview into infowindow
+    var pano = null;
+    google.maps.event.addListener(infowindow, 'domready', function() {
+        if (pano !== null) {
+            pano.unbind("position");
+            pano.setVisible(false);
+        }
+        pano = new google.maps.StreetViewPanorama(document.getElementById("content"), {
+            navigationControl: true,
+            navigationControlOptions: {
+                style: google.maps.NavigationControlStyle.ANDROID
+            },
+            enableCloseButton: false,
+            addressControl: false,
+            linksControl: false
+        });
+        pano.bindTo("position", this);
+        pano.setVisible(true);
+    });
 
+    google.maps.event.addListener(infowindow, 'closeclick', function() {
+        pano.unbind("position");
+        pano.setVisible(false);
+        pano = null;
+    });
     /* ========= View model taht work whit knockout js =========*/
     var ViewModel = function() {
         var self = this;
         self.title = ko.observable('Store and Restaurant');
-        //Click on item in list view
-        self.listViewClick = function(gym) {
-            map.setZoom(18);
-            map.setCenter(this.Marker.marker.getPosition());
-        };
-
     };
-
     /* ========= Call the functions ViewModel =========*/
     ko.applyBindings(new ViewModel());
 }
